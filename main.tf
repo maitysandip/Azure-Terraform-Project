07cbde6e6057fa0d4ffcd2615f4f4fdad2edf1aa
Sandip Maity <sandip.maity@adani.com>
2024-11-15 11:33:34 +0530

    Added Network Security Group

--- main.tf
+++ main.tf
@@ -32,8 +32,18 @@ resource "azurerm_virtual_network" "mtc-vn" {
 }
 
 resource "azurerm_subnet" "mtc-subnet" {
-    name = "mtc-subnet"
-    resource_group_name = azurerm_resource_group.mtc-rg.name
-    virtual_network_name = azurerm_virtual_network.mtc-vn.name
-    address_prefixes     = ["10.123.1.0/24"]
+  name                 = "mtc-subnet"
+  resource_group_name  = azurerm_resource_group.mtc-rg.name
+  virtual_network_name = azurerm_virtual_network.mtc-vn.name
+  address_prefixes     = ["10.123.1.0/24"]
+}
+
+resource "azurerm_network_security_group" "mtc-sg" {
+  name                = "mtc-sg"
+  location            = azurerm_resource_group.mtc-rg.location
+  resource_group_name = azurerm_resource_group.mtc-rg.name
+
+  tags = {
+    environment = "dev"
+  }
 }
