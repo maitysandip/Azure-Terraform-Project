1835ba7ccec887d27b772cbbe508133eca9a5f9f
Sandip Maity <sandip.maity@adani.com>
2024-11-16 14:07:56 +0530

    Added Variables

--- main.tf
+++ main.tf
@@ -135,7 +135,7 @@ resource "azurerm_linux_virtual_machine" "mtc-vm" {
 
   provisioner "local-exec" {
     command = templatefile(
-      "windows-ssh-script.tpl",
+      "${var.host_os}-ssh-script.tpl",
       {
         hostname     = self.public_ip_address,
         user         = "adminuser",
